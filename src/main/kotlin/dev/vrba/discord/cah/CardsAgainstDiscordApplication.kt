ee8ba01b16f792317f7eecef8fc6185b094e3084
Jiří Vrba <jiri@vrba.dev>
2022-05-21 13:15:06 +0200

    Configure jackson kotlin module

--- src/main/kotlin/dev/vrba/discord/cah/CardsAgainstDiscordApplication.kt
+++ src/main/kotlin/dev/vrba/discord/cah/CardsAgainstDiscordApplication.kt
@@ -1,10 +1,28 @@
 package dev.vrba.discord.cah
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinFeature
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class CardsAgainstDiscordApplication
+class CardsAgainstDiscordApplication {
+    @Bean
+    fun objectMapper(): ObjectMapper {
+        return ObjectMapper().registerModule(
+            KotlinModule.Builder()
+                .withReflectionCacheSize(512)
+                .configure(KotlinFeature.NullToEmptyCollection, false)
+                .configure(KotlinFeature.NullToEmptyMap, false)
+                .configure(KotlinFeature.NullIsSameAsDefault, false)
+                .configure(KotlinFeature.SingletonSupport, false)
+                .configure(KotlinFeature.StrictNullChecks, false)
+                .build()
+        )
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<CardsAgainstDiscordApplication>(*args)
