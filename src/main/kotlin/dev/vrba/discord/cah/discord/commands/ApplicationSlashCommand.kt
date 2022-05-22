1469659df96911d11ebfc9052f659b5fe02cbd80
Jiří Vrba <jiri@vrba.dev>
2022-05-22 15:37:23 +0200

    Add execute method to the application slash command interface

--- src/main/kotlin/dev/vrba/discord/cah/discord/commands/ApplicationSlashCommand.kt
+++ src/main/kotlin/dev/vrba/discord/cah/discord/commands/ApplicationSlashCommand.kt
@@ -1,9 +1,12 @@
 package dev.vrba.discord.cah.discord.commands
 
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData
 
 interface ApplicationSlashCommand {
 
     fun define(): SlashCommandData
+
+    fun execute(event: SlashCommandInteractionEvent)
 
 }
